63868bcc9fdaafa87991f0a4124dab412fcd469e
matondojoao <matondojoaokitemoco@gmail.com>
2023-11-16 12:47:55 +0100

    API de Cursos no Laravel

---
+++ app/Models/Course.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Traits\UuidTrait;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Course extends Model
+{
+    use HasFactory, UuidTrait;
+
+    protected $table="courses";
+
+    protected $filbale=['name','description','image'];
+}
--- routes/api.php
+++ routes/api.php
@@ -1,21 +1,9 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\Api\CourseController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
 
-Route::get('/', function(){
-   return response()->json([
-    'success'=>true
-   ]);
+Route::controller(CourseController::class)->prefix('courses')->group(function () {
+    Route::get('/', 'index');
 });
